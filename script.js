fe0fb9d5e032456dfdb90a5c12bf912954662e70
iammmei <safae.khess@gmail.com>
2023-06-20 10:51:32 +0200

    on y travaille

--- script.js
+++ script.js
@@ -1,31 +1,80 @@
 let products = {
-    data:[{
-        productName: "Regular white shirt ",
-        category: "topwear",
-        price: "30",
-        image:"whiteshirt.jfif",
+  data: [
+    {
+      productName: "Regular white shirt ",
+      category: "Topwear",
+      price: "30",
+      image: "whiteshirt.jfif",
     },
     {
-        productName: "Regular white shirt ",
-        category: "topwear",
-        price: "30",
-        image:"whiteshirt.jfif",
+      productName: "Beige Short Skirt  ",
+      category: "Bottomwear",
+      price: "30",
+      image: "beigeshort.jpg",
     },
     {
-        productName: "Regular white shirt ",
-        category: "topwear",
-        price: "30",
-        image:"whiteshirt.jfif",
+      productName: "Sporty SmartWatch",
+      category: "Watch",
+      price: "99",
+      image: "sporty-smartwatch. jpg",
     },
     {
-        productName: "Regular white shirt ",
-        category: "topwear",
-        price: "30",
-        image:"whiteshirt.jfif",
-    }
-    
+      productName: "Basic Knitted Top",
+      category: "Topwear",
+      price: "29",
+      image: "knitted-top.jpg",
+    },
+
+    {
+      productName: "Black Leather Jacket",
+      category: "Topwear",
+      price: "29",
+      image: "black-leather.jpg",
+    },
+  ],
+};
+
+for(let i of products.data) {
+//Create card
+    let card = document.createElement("div")
+//Adding category and classs
+card.classList.add("card", "i.category");
+
+//img div 
+let imgContainer= document.createElement("div");
+imgContainer.classList.add("image-container");
+
+//img tag
+
+let image= document.createElement("img");
+image.setAttribute("src",i.image);
+imgContainer.appendChild(image);
+card.appendChild(imgContainer);
+//container
+let container = document.createElement("div")
+container.classList.add("container")
+//product name 
+let name = document.creayeElement("h5");
+name.classList.add("product-name")
+name.innerText = i.productName.toUpperCase();
+container.appendChild(name);
+
+//price
+
+let price = document.createElement("h6")
+price.innerText =i.price;
+container.appendChild(""+ price);
 
 
 
-}]
+card.appendChild(container);
+
+
+document.getElementById("products").appendChild(card);
+
+
+
+
+
+
 }
