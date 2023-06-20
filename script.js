3973d7249dfb828cd730b6a229264496680b07c2
iammmei <safae.khess@gmail.com>
2023-06-20 12:32:08 +0200

    end but doesnt display a thing

--- script.js
+++ script.js
@@ -37,8 +37,8 @@ let products = {
 for (let i of products.data) {
   //Create card
   let card = document.createElement("div");
-  //Adding category and classs
-  card.classList.add("card", "i.category");
+  //Adding category and classs and stay hidden initiallyy
+  card.classList.add("card", "i.category", "hide");
 
   //img div
   let imgContainer = document.createElement("div");
@@ -69,3 +69,58 @@ for (let i of products.data) {
 
   document.getElementById("products").appendChild(card);
 }
+
+//parameter passed from  button (parameter same as category)
+function filterProduct(value) {
+  //button class code
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    // check if value equals innerText
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  // select all cards
+  let elements = document.querySelectorAll(".card");
+  //loop through all cards
+
+  elements.forEach((element) => {
+    if (value == "all") {
+      element.classList.remove("hide");
+    } else {
+      //check if element contains category class
+      if (element.classList.contains(value)) {
+        //displays element based on category
+        element.classList.remove("hide");
+      } else {
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+  //initializations
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+  // loop through all elements
+  elements.forEach((element, index) => {
+    // check if text includes the search value
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      cards[index].classList.remove("hide");
+    } else {
+      //hide others
+      cards[index].classList.add("hide");
+    }
+  });
+});
+
+//Initially display all products
+window.onload = () => {
+  filterProduct("all");
+};
